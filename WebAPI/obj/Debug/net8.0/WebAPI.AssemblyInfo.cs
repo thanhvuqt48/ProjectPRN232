ffa942e72300d10830a13f23b03f3216afad736f
HoangHaiDev27 <hoanghai270623@gmail.com>
2025-07-03 17:14:30 +0700

    push setup model

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fe25ac6feab37639a9e8caba9e52719804aae02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8417b6d592ff9ba49866d6a06c628b0064a06f12")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
